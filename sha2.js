07b9568a916f2c1c06f07809ed32aecbbc4b1d3b
lavenderstream <136625214@qq.com>
2016-09-09 15:42:45 +0800

    first commit

--- sha2.js
+++ sha2.js
@@ -2,7 +2,7 @@ var filename = process.argv[2];
 var crypto = require('crypto');
 var fs = require('fs');
 var colors = require('colors');
-var shasum = crypto.createHash('sha256');
+var shasum = crypto.createHash('sha256'); 
 
 var s = fs.ReadStream(filename);
 s.on('data', function(d) {
